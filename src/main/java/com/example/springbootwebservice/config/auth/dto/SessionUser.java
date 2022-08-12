1ca24defcb23642576f66c256d471da6181080b0
Kimhyeyun <cqqwer@gmail.com>
2022-08-13 01:16:10 +0900

    SessionUser implementation Serializable update

--- src/main/java/com/example/springbootwebservice/config/auth/dto/SessionUser.java
+++ src/main/java/com/example/springbootwebservice/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package com.example.springbootwebservice.config.auth.dto;
 import com.example.springbootwebservice.domain.user.Users;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
