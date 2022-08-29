afb5f253796c7a05b6f8e4f478f6e5f29ce490cb
Kimhyeyun <cqqwer@gmail.com>
2022-08-29 15:29:00 +0900

    deploy.sh update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
-# !/bin/bash
+#!/bin/bash
 
 REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=spring-boot-webservice
@@ -13,15 +13,16 @@ CURRENT_PID=$(pgrep -fl springboot-webservice-practice | grep java | awk '{print
 
 echo "현재 구동 중인 애플리케이션 pid : $CURRENT_PID"
 
-if [ -z "$CURRENT_PID"]; then
+if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
+  kill -15 "$CURRENT_PID"
   sleep 5
 fi
 
 echo "> 새 애플리케이션 배포"
+
 
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
@@ -29,11 +30,11 @@ echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권환 추가"
 
-chmod +x $JAR_NAME
+chmod +x "$JAR_NAME"
 
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
   -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties, /home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties  \
   -Dspring.profiles.active=real \
-  $JAR_NAME > $REPOSITORY/nohup.out 2>1 &
+  "$JAR_NAME" > $REPOSITORY/nohup.out 2>1 &
